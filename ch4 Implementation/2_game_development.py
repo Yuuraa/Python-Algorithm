517dcb29d6c2096d0c0b9bc082b1c6ffc5e9b8f3
Yuuraa <yoorachoi8937@gmail.com>
2021-01-20 22:05:43 +0900

    Chapter4 Implementation

--- ch4 Implementation/2_game_development.py
+++ ch4 Implementation/2_game_development.py
@@ -1,20 +1,34 @@
 m, n = map(int, input().split())
 r, c, direction = map(int, input().split())
-game_map = []
 
 dr = [-1, 0, 1, 0]
 dc = [0, 1, 0, -1]
 
-for i in range(m):
-    game_map.append(list(input().split()))
+game_map = []
+for _ in range(m):
+    game_map.append(list(map(int, input().split())))
 
-while True:
-    for _  in range(4):
-        left_dir = ((direction - i - 1) + 4)%4
-        nr, nc = r + dr[left_dir], c + dc[left_dir]
-        if nr < 0 or nc < 0 or nr >= m or nc >= n:
-            continue
-        else:
+count, playing = 0, True
+while playing:
+    progress = False
+    for i in range(4):
+        left_direction = (direction - i + 3)%4
+        nr, nc = r + dr[left_direction], c + dc[left_direction]
+        if nr >=0 and nc >= 0 and nr < m and nc < n:
             if game_map[nr][nc] == 0:
-                game_map[nr][nc]
+                game_map[nr][nc] = 2
+                progress = True
+                r, c = nr, nc
+                direction = left_direction
+                break
+    if progress:
+        count += 1
+    else:
+        back_direction = (direction + 2)%4
+        nr, nc = r + dr[back_direction], c + dc[back_direction]
+        if nr >=0 and nc >= 0 and nr < m and nc < n:
+            if game_map[nr][nc] == 1:
+                playing = False
+            else: r, c = nr, nc
 
+print(count)
--- ch4 Implementation/2_number_card.py
+++
@@ -1,9 +0,0 @@
-N, M = map(int, input().split())
-card_nums = [list(map(int, input().split())) for _ in range(N)]
-
-answer = 0
-for r in range(N):
-    # min_num = min([card_nums[i][c] for i in range(N)])
-    min_num = min(card_nums[r])
-    answer = max(min_num, answer)
-print(answer)
