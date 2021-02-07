845a1ac959e14800aa5db3b9da56b5f69ef69e11
Yuuraa <yoorachoi8937@gmail.com>
2021-02-07 18:47:20 +0900

    Add better sol to 0207 Shortest Distance to a Character

--- Leetcode/FEB_2020/shortest_to_character.py
+++ Leetcode/FEB_2020/shortest_to_character.py
@@ -14,7 +14,25 @@ def shortest_to_char(s, c):
     return [min_dist(i, char) for i, char in enumerate(s)]
 
 
+def shortest_to_char_better(s, c):
+    prev, answer = -(len(s) + 1), []
+    
+    for i, char in enumerate(s):
+        if char == c: prev = i
+        answer.append(i - prev)
+    
+    for i in range(len(s) - 1, -1, -1):
+        if s[i] == c: prev = i
+        answer[i] = min(answer[i], prev - i)
+
+    return answer
+
+
+
 if __name__ == "__main__":
     assert(shortest_to_char("loveleetcode", "e") == [3,2,1,0,1,0,0,1,2,2,1,0])
     assert(shortest_to_char("aaab", "b") == [3,2,1,0])
     print("All samples passed")
+    assert(shortest_to_char_better("loveleetcode", "e") == [3,2,1,0,1,0,0,1,2,2,1,0])
+    assert(shortest_to_char_better("aaab", "b") == [3,2,1,0])
+    print("All samples passed2")
